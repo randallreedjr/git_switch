4448eee3b117f3af55e1452aded9fb82c36958bc
Randall Reed, Jr <randallreedjr@gmail.com>
2018-07-16 14:07:42 -0400

    Set profile form command line

--- lib/git_switch.rb
+++ lib/git_switch.rb
@@ -1,5 +1,20 @@
-require "git_switch/version"
+# require "git_switch/version"
+require 'yaml'
 
-module GitSwitch
+class GitSwitch
   # Your code goes here...
+  def set(username)
+    # puts `git config -l`
+    config = YAML.load_file(File.expand_path('~/.gitswitch'))
+    `git config user.name "#{config[username]["name"]}"`
+    `git config user.username "#{config[username]["username"]}"`
+    `git config user.email "#{config[username]["email"]}"`
+  end
+end
+
+switch = GitSwitch.new
+if ARGV.length == 1
+  switch.set(ARGV.first)
+else
+  puts "Please provide the profile to use"
 end
